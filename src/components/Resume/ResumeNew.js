edf2418058a5f40c8a67d4237839db5959c4557f
Rinku921234 <rinku921234@gmail.com>
2023-01-23 14:07:14 +0530

    resume Add

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -9,7 +9,7 @@ import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://drive.google.com/file/d/1nqqpFB4RDAHMXkKd4FBTfonjqLJc9vUh/view?usp=sharing";
+  "https://drive.google.com/file/d/1MWBIrljWTqRdsVXq2yghTbeSS8Z7VfcZ/view?usp=share_link";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
